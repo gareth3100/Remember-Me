adf2449342d3a6ba223330277eac0965f4dbeb2b
Dillon Lee <dichlee@ucsc.edu>
2021-06-02 04:33:58 -0700

    added a star in the navigation header

---
+++ src/components/CreateContactComponet/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../assets/theme/colors';
+
+export default StyleSheet.create({
+    container: {flex: 1, backgroundColor: 'white'},
+
+    imageView:{
+        width: 150,
+        height: 150,
+        borderRadius: 100,
+        alignSelf: "center",
+    },
+
+    chooseText: {
+        color: colors.primary,
+        textAlign: 'center',
+    },
+
+    inputText: {
+        height: 300,
+        fontSize: 30
+    },
+});
---
+++ src/screens/FacialRecognition/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text,View } from 'react-native';
+
+const FacialRecognition = () => {
+    return(
+        <View>
+            <Text>Hello from Facial Recognition!</Text>
+        </View>
+    );
+};
+
+export default FacialRecognition;
